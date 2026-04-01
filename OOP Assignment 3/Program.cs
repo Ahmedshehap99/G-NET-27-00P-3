dc6750dc9c4be898a4b02d2ce00135726e7ec137
AhmedShehap99 <55rrtyy7@gmail.com>
2026-04-01 07:17:36 +0100

    Question 02

--- OOP Assignment 3/Program.cs
+++ OOP Assignment 3/Program.cs
@@ -19,6 +19,41 @@
             //    e) Dependency 
             #endregion
 
+            #region Question 02
+            //Question 02
+
+
+
+
+            //        a)
+
+            //        Yes, a child class in a different assembly can access a protected member if it inherits from the parent.
+
+            //        It cannot be accessed from outside the class via an object instance.
+
+
+            //        b)
+
+            //        protected internal → Accessible within the same assembly or via inheritance.
+
+
+            //        private protected → Accessible only within the same assembly and via inheritance.
+
+
+            //        c)
+
+            //        sealed class → Cannot be inherited from.
+
+            //        sealed method → ​​Cannot be overridden in a child class.
+
+
+            //        d)
+
+            //        Yes, an object can be created from a sealed class using `new`.
+
+            //        However, it cannot inherit from a sealed class. 
+            #endregion
+
 
 
 
